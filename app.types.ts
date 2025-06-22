549d164409c2920d31687a05826c5557c13cf3c5
MustafoAlisherovich <mustafoalisherovic@gmail.com>
2025-06-22 22:51:03 +0500

    Sections & Lessons

--- app.types.ts
+++ app.types.ts
@@ -41,6 +41,13 @@ export interface ILesson {
 		minutes: number
 		seconds: number
 	}
+	userProgress: IUserProgress[]
+}
+
+export interface IUserProgress {
+	userId: string
+	lessonId: string
+	isCompleted: string
 }
 
 export interface IUser {
--- app/[lng]/dashboard/[courseId]/_components/sidebar.tsx
+++ app/[lng]/dashboard/[courseId]/_components/sidebar.tsx
@@ -23,12 +23,12 @@ async function Sidebar({ courseId, lng }: Props) {
 				<h1 className='line-clamp-1 text-xl font-medium'>{course.title}</h1>
 				<Progress value={progressPercentage} className='h-4' />
 				<p className='text-sm'>
-					{progressPercentage}% {t('completed')}
+					{progressPercentage.toFixed(0)}% {t('completed')}
 				</p>
 			</div>
 
 			<div className='mt-4'>
-				<Sections sections={sections} />
+				<Sections sections={JSON.parse(JSON.stringify(sections))} />
 			</div>
 		</div>
 	)
